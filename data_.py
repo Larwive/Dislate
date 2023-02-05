6a7f0698f3321941b37ef75ebd6d05140bea8232
Larwive <larwive@gmail.com>
2023-02-05 13:54:31 +0100

    Ajouts.

--- data_.py
+++ data_.py
@@ -1,3 +1,4 @@
+chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 dic = {"BG": "Bulgarian",
   "CS": "Czech",
   "DA": "Danish",
@@ -31,4 +32,31 @@ dic = {"BG": "Bulgarian",
 authorized_channels = [837595468564529192, 1068960817615229080]
 auth_languages = ["EN-GB", "EN-GB"]
 
-help_text="**BG** - Bulgarian\n**CS** - Czech\n**DA** - Danish\n**DE** - German\n**EL** - Greek\n**EN-GB** - English (British)\n**EN-US** - English (American)\n**ES** - Spanish\n**ET** - Estonian\n**FI** - Finnish\n**FR** - French\n**HU** - Hungarian\n**ID** - Indonesian\n**IT** - Italian\n**JA** - Japanese\n**LT** - Lithuanian\n**LV** - Latvian\n**NL** - Dutch\n**PL** - Polish\n**PT-BR** - Portuguese (Brazilian)\n**PT-PT** - Portuguese (all Portuguese varieties excluding Brazilian Portuguese)\n**RO** - Romanian\n**RU** - Russian\n**SK** - Slovak\n**SL** - Slovenian\n**SV** - Swedish\n**TR** - Turkish\n**UK** - Ukrainian\n**ZH** - Chinese (simplified)"
+help_text_target="**BG** - Bulgarian\n**CS** - Czech\n**DA** - Danish\n**DE** - German\n**EL** - Greek\n**EN-GB** - English (British)\n**EN-US** - English (American)\n**ES** - Spanish\n**ET** - Estonian\n**FI** - Finnish\n**FR** - French\n**HU** - Hungarian\n**ID** - Indonesian\n**IT** - Italian\n**JA** - Japanese\n**KO** - Korean\n**LT** - Lithuanian\n**LV** - Latvian\n**NL** - Dutch\n**PL** - Polish\n**PT-BR** - Portuguese (Brazilian)\n**PT-PT** - Portuguese (all Portuguese varieties excluding Brazilian Portuguese)\n**RO** - Romanian\n**RU** - Russian\n**SK** - Slovak\n**SL** - Slovenian\n**SV** - Swedish\n**TR** - Turkish\n**UK** - Ukrainian\n**ZH** - Chinese (simplified)"
+
+help_text_source="**BG** - Bulgarian\n**CS** - Czech\n**DA** - Danish\n**DE** - German\n**EL** - Greek\n**EN** - English\n**ES** - Spanish\n**ET** - Estonian\n**FI** - Finnish\n**FR** - French\n**HU** - Hungarian\n**ID** - Indonesian\n**IT** - Italian\n**JA** - Japanese\n**KO** - Korean\n**LT** - Lithuanian\n**LV** - Latvian\n**NL** - Dutch\n**PL** - Polish\n**PT** - Portuguese\n**RO** - Romanian\n**RU** - Russian\n**SK** - Slovak\n**SL** - Slovenian\n**SV** - Swedish\n**TR** - Turkish\n**UK** - Ukrainian\n**ZH** - Chinese (simplified)"
+
+quotes = [
+  "«Yoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow», Yanis, 01/02/2023 à 14h05",
+  "«Wesh mon N*gros», Maximilien, tous les jours.",
+  "«J'étais sur Tik Tok», Ilyes, tous les jours.",
+  "«Je dors pas tant que j'ai pas atteint 400 d'élo», Ilyes, il y a un certain temps.",
+  "«Oula», Yanis, 01/02/2023 à 22h44.",
+  "«Le jaune c'est le noir», Floréal, 02/02/2023 vers 11h",
+  "«Pause café !», Abdu, tous les jours",
+  "«Tu connais Warhammer ?», Kenzo, souvent",
+  "«Tu veux visiter ma cave ?», Kévin, 10% de ses paroles chaque jour",
+  "«Je touche le plafond», Maximilien, todos los dias",
+  "«C'est un booléen la note ?», Abdu, tireur d'élite",
+  "«Elle renvoie quoi la fonction ?», Denis, occasionellement",
+  "«Mettez une flamme pour among us», Maximilien, du 13/01/2023 au 04/02/2023",
+  "«Moi je fais la technique du sous-marin.», Maximilien, 03/02/2023 à 9h17",
+  "«T'as vu il dit le n-word le chat.», Ilyes, 03/02/2023 à 13h25",
+  "«Quelqu'un se met en vert clair s'il vous plaît.», vers Clair, 03/02/2023 sur Among Us",
+  "«Je pense c'est Alexandre. Il a survécu un tour.», Pyrhanox, 03/02/2023 sur Among Us",
+  #"«Je pense c'est Kévin.», Pyrhanox, 03/02/2023 sur Among Us",
+  "«Moi je pense..., chais pas.», Pyrhanox, 03/02/2023 sur Among Us",
+  "«Laissez-moi vivre le premier tour.», PotatOwO, 03/02/2023 sur Among Us",
+  "«Il m'a kill dans Abdu, frère.», Maxlener, 03/02/2023 sur Among Us",
+  "«Et là y'a Kenzo qui danse devant les caméras.», tout le monde, 03/02/2023 sur Among Us"
+]
--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import deepl
-#from keep_alive import keep_alive
+from random import randint
+import requests
+import json
+# from keep_alive import keep_alive
 import discord
-# from discord import app_commands
+from discord import app_commands
 import os
 from discord.ext import commands
 from datetime import datetime
-from data_ import authorized_channels, auth_languages, dic, help_text
+from data_ import authorized_channels, auth_languages, dic, help_text_source, help_text_target, quotes, chars
+
+gifs_number = 100
+all_gifs = {}
+
+guilds = [discord.Object(743319254107029584), discord.Object(1021336800079380480)]
 
 intents = discord.Intents.all()
-"""
-# Pour les commandes slash
-client = discord.Client(intents=intents)
-tree = app_commands.CommandTree(client)"""
 
 bot = commands.Bot(command_prefix=".", intents=intents)  # Change le préfix ici
+bot.remove_command('help')
 # slash = SlashCommand(bot, sync_commands=True)
+# Pour les commandes slash
+# client = discord.Client(intents=intents)
+# tree = app_commands.CommandTree(bot)
 
 translator = deepl.Translator(str(os.environ.get("DEEPL")))
 print("Translator connected.")
@@ -36,7 +44,7 @@ async def tr(ctx, target_language):
         await ctx.send(embed=embed)
         print(ctx.message.author)
     except:
-        await ctx.send(translator.translate_text("Error while trying to translate.", target_lang=tl))
+        await ctx.send("Error while trying to translate.")
 
 
 @bot.command()
@@ -67,22 +75,241 @@ async def on_message(message):
 @bot.command()
 async def randomen(ctx):
   await ctx.send("This brings you to a random page of Bulbapedia : https://bulbapedia.bulbagarden.net/wiki/Special:Random")
+"""
 
 
-@tree.command(name = "tr", description = "My first application Command", guild=discord.Object(id=743319254107029584))
-async def first_command(interaction, target_language:str):
-  #try:
-  print(0)
-  message = await interaction.channel.fetch_message(interaction.message.reference.message_id)
-  print(1)
-  result = translator.translate_text(message.content, target_lang=str(target_language))
-  print(2)
-  await interaction.response.send_message(result.text)
-  #except:
-  await interaction.response.send_message("Error while trying to translate.")
-  #await interaction.response.send_message("Hello!")
+@bot.tree.command(name="translate", description="A translator using Deepl.", guilds=guilds)
+async def translate(interaction, text: str, target_language: str, source_language: str = "EN"):
+    try:
+        result = translator.translate_text(text, source_lang=str(source_language).upper(),
+                                           target_lang=str(target_language).upper())
+        await interaction.response.send_message(result.text)
+    except:
+        await interaction.response.send_message("Error while trying to translate.")
 
-@tree.command(name = "help", description = "The command that helps you use dislate.", guilds=[discord.Object(743319254107029584), discord.Object(1021336800079380480)])"""
+
+@bot.tree.command(name="ping", description="A command to use every day.", guilds=guilds)
+async def ping(interaction):
+    try:
+        await interaction.response.send_message("<@360439679775932416>")
+    except:
+        pass
+
+
+@bot.tree.command(name="pat", description="Pat someone", guilds=guilds)
+async def pat(interaction, user: discord.User):
+    embed = discord.Embed(description="<@{}> pats <@{}> !".format(interaction.user.id, user.id), colour=0xB991FF,
+                          timestamp=datetime.utcnow())
+    embed.set_footer(text="Dislate")
+    number = randint(0, 49)
+    embed.set_image(url=all_gifs["pat"]["results"][number]["media_formats"]["gif"]["url"])
+    await interaction.response.send_message(embed=embed)
+    requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(
+        all_gifs["pat"]["results"][number]["id"], os.environ.get("TENOR"), "Dislate", "pat"))
+
+
+@bot.tree.command(name="kiss", description="Kiss someone", guilds=guilds)
+async def kiss(interaction, user: discord.User):
+    embed = discord.Embed(description="<@{}> kisses <@{}> !".format(interaction.user.id, user.id), colour=0xB991FF,
+                          timestamp=datetime.utcnow())
+    embed.set_footer(text="Dislate")
+    number = randint(0, 49)
+    embed.set_image(url=all_gifs["kiss"]["results"][number]["media_formats"]["gif"]["url"])
+    await interaction.response.send_message(embed=embed)
+    requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(
+        all_gifs["kiss"]["results"][number]["id"], os.environ.get("TENOR"), "Dislate", "kiss"))
+
+
+@bot.tree.command(name="hit", description="Hit someone", guilds=guilds)
+async def hit(interaction, user: discord.User):
+    embed = discord.Embed(description="<@{}> hits <@{}> !".format(interaction.user.id, user.id), colour=0xB991FF,
+                          timestamp=datetime.utcnow())
+    embed.set_footer(text="Dislate")
+    number = randint(0, 49)
+    embed.set_image(url=all_gifs["hit"]["results"][number]["media_formats"]["gif"]["url"])
+    await interaction.response.send_message(embed=embed)
+    requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(
+        all_gifs["hit"]["results"][number]["id"], os.environ.get("TENOR"), "Dislate", "hit"))
+
+
+@bot.tree.command(name="slap", description="Slap someone", guilds=guilds)
+async def slap(interaction, user: discord.User):
+    embed = discord.Embed(description="<@{}> slaps <@{}> !".format(interaction.user.id, user.id), colour=0xB991FF,
+                          timestamp=datetime.utcnow())
+    embed.set_footer(text="Dislate")
+    number = randint(0, 49)
+    embed.set_image(url=all_gifs["slap"]["results"][number]["media_formats"]["gif"]["url"])
+    await interaction.response.send_message(embed=embed)
+    requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(
+        all_gifs["slap"]["results"][number]["id"], os.environ.get("TENOR"), "Dislate", "slap"))
+
+
+@bot.tree.command(name="happy", description="Show that you're happy.", guilds=guilds)
+async def happy(interaction):
+    embed = discord.Embed(description="<@{}> is happy !".format(interaction.user.id), colour=0xB991FF,
+                          timestamp=datetime.utcnow())
+    embed.set_footer(text="Dislate")
+    number = randint(0, 49)
+    embed.set_image(url=all_gifs["happy"]["results"][number]["media_formats"]["gif"]["url"])
+    await interaction.response.send_message(embed=embed)
+    requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(
+        all_gifs["happy"]["results"][number]["id"], os.environ.get("TENOR"), "Dislate", "happy"))
+
+
+@bot.tree.command(name="sad", description="Show that you're sad", guilds=guilds)
+async def sad(interaction):
+    embed = discord.Embed(description="<@{}> is sad !".format(interaction.user.id), colour=0xB991FF,
+                          timestamp=datetime.utcnow())
+    embed.set_footer(text="Dislate")
+    number = randint(0, 49)
+    embed.set_image(url=all_gifs["sad"]["results"][number]["media_formats"]["gif"]["url"])
+    await interaction.response.send_message(embed=embed)
+    requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(
+        all_gifs["sad"]["results"][number]["id"], os.environ.get("TENOR"), "Dislate", "sad"))
+
+
+@bot.tree.command(name="angry", description="Show that you're angry.", guilds=guilds)
+async def angry(interaction, user: discord.User):
+    embed = discord.Embed(description="<@{}> is angry at <@{}> !".format(interaction.user.id, user.id), colour=0xB991FF,
+                          timestamp=datetime.utcnow())
+    embed.set_footer(text="Dislate")
+    number = randint(0, 49)
+    embed.set_image(url=all_gifs["angry"]["results"][number]["media_formats"]["gif"]["url"])
+    await interaction.response.send_message(embed=embed)
+    requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(
+        all_gifs["angry"]["results"][number]["id"], os.environ.get("TENOR"), "Dislate", "angry"))
+
+
+@bot.tree.command(name="hungry", description="Are you hungry enough to use this command ?", guilds=guilds)
+async def hungry(interaction):
+    embed = discord.Embed(description="<@{}> is hungry...".format(interaction.user.id), colour=0xB991FF,
+                          timestamp=datetime.utcnow())
+    embed.set_footer(text="Dislate")
+    number = randint(0, 49)
+    embed.set_image(url=all_gifs["hungry"]["results"][number]["media_formats"]["gif"]["url"])
+    await interaction.response.send_message(embed=embed)
+    requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(
+        all_gifs["hungry"]["results"][number]["id"], os.environ.get("TENOR"), "Dislate", "hungry"))
+
+
+@bot.tree.command(name="jealous", description="Are you jealous of someone ?", guilds=guilds)
+async def jealous(interaction, user: discord.User):
+    embed = discord.Embed(description="<@{}> is jealous of <@{}>.".format(interaction.user.id, user.id),
+                          colour=0xB991FF,
+                          timestamp=datetime.utcnow())
+    embed.set_footer(text="Dislate")
+    number = randint(0, 49)
+    embed.set_image(url=all_gifs["jealous"]["results"][number]["media_formats"]["gif"]["url"])
+    await interaction.response.send_message(embed=embed)
+    requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(
+        all_gifs["jealous"]["results"][number]["id"], os.environ.get("TENOR"), "Dislate", "jealous"))
+
+
+@bot.tree.command(name="innocent", description="Are you truly innocent to invoke this command ?", guilds=guilds)
+async def innocent(interaction):
+    embed = discord.Embed(description="<@{}> is innocent... really ?".format(interaction.user.id), colour=0xB991FF,
+                          timestamp=datetime.utcnow())
+    embed.set_footer(text="Dislate")
+    number = randint(0, 49)
+    embed.set_image(url=all_gifs["innocent"]["results"][number]["media_formats"]["gif"]["url"])
+    await interaction.response.send_message(embed=embed)
+    requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(
+        all_gifs["innocent"]["results"][number]["id"], os.environ.get("TENOR"), "Dislate", "innocent"))
+
+
+@bot.tree.command(name="bigbraintime", description="It's big brain time !", guilds=guilds)
+async def bigbraintime(interaction):
+    embed = discord.Embed(description="<@{}> is having a big brain time !".format(interaction.user.id), colour=0xB991FF,
+                          timestamp=datetime.utcnow())
+    embed.set_footer(text="Dislate")
+    number = randint(0, 49)
+    embed.set_image(url=all_gifs["big brain time"]["results"][number]["media_formats"]["gif"]["url"])
+    await interaction.response.send_message(embed=embed)
+    requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(
+        all_gifs["big brain time"]["results"][number]["id"], os.environ.get("TENOR"), "Dislate", "big brain time"))
+
+
+@bot.tree.command(name="sus", description="Psst, tell me who's sus.", guilds=guilds)
+async def sus(interaction, user: discord.User):
+    embed = discord.Embed(description="<@{}> is sus.".format(user.id), colour=0xB991FF,
+                          timestamp=datetime.utcnow())
+    embed.set_footer(text="Dislate")
+    number = randint(0, 49)
+    embed.set_image(url=all_gifs["sus"]["results"][number]["media_formats"]["gif"]["url"])
+    await interaction.response.send_message(embed=embed)
+    requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(
+        all_gifs["sus"]["results"][number]["id"], os.environ.get("TENOR"), "Dislate", "sus"))
+
+
+@bot.tree.command(name="cringe", description="Looks like someone is cringe.", guilds=guilds)
+async def cringe(interaction, user: discord.User):
+    embed = discord.Embed(description="<@{}> is so cringe.".format(user.id), colour=0xB991FF,
+                          timestamp=datetime.utcnow())
+    embed.set_footer(text="Dislate")
+    number = randint(0, 49)
+    embed.set_image(url=all_gifs["cringe"]["results"][number]["media_formats"]["gif"]["url"])
+    await interaction.response.send_message(embed=embed)
+    requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(
+        all_gifs["cringe"]["results"][number]["id"], os.environ.get("TENOR"), "Dislate", "cringe"))
+
+
+@bot.tree.command(name="strange", description="To use when someone is acting strange.", guilds=guilds)
+async def strange(interaction, user: discord.User):
+    embed = discord.Embed(description="<@{}> is acting strange...".format(user.id), colour=0xB991FF,
+                          timestamp=datetime.utcnow())
+    embed.set_footer(text="Dislate")
+    number = randint(0, 49)
+    embed.set_image(url=all_gifs["strange"]["results"][number]["media_formats"]["gif"]["url"])
+    await interaction.response.send_message(embed=embed)
+    requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(
+        all_gifs["strange"]["results"][number]["id"], os.environ.get("TENOR"), "Dislate", "strange"))
+
+
+@bot.tree.command(name="dab", description="Dab whenever you want.", guilds=guilds)
+async def dab(interaction, user: discord.User):
+    embed = discord.Embed(description="<@{}> dabs !".format(user.id), colour=0xB991FF,
+                          timestamp=datetime.utcnow())
+    embed.set_footer(text="Dislate")
+    number = randint(0, 49)
+    embed.set_image(url=all_gifs["dab"]["results"][number]["media_formats"]["gif"]["url"])
+    await interaction.response.send_message(embed=embed)
+    requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(
+        all_gifs["dab"]["results"][number]["id"], os.environ.get("TENOR"), "Dislate", "dab"))
+
+
+@bot.tree.command(name="dealer", description="When you want to report a dealer.", guilds=guilds)
+async def dealer(interaction, user: discord.User):
+    embed = discord.Embed(description="<@{}> got some deals !".format(user.id), colour=0xB991FF,
+                          timestamp=datetime.utcnow())
+    embed.set_footer(text="Dislate")
+    number = randint(0, 49)
+    embed.set_image(url=all_gifs["dealer"]["results"][number]["media_formats"]["gif"]["url"])
+    await interaction.response.send_message(embed=embed)
+    requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(
+        all_gifs["dealer"]["results"][number]["id"], os.environ.get("TENOR"), "Dislate", "dealer"))
+
+
+# @tree.command(name = "help", description = "The command that helps you use dislate.", guilds=[discord.Object(743319254107029584), discord.Object(1021336800079380480)])
+# """"""
+
+@bot.tree.command(name="randomquote", description="Sends a random quote", guilds=guilds)
+async def quote(interaction):
+    await interaction.response.send_message(quotes[randint(0, len(quotes) - 1)])
+
+
+@bot.tree.command(name="convert", description="Convert any base from 2 to 36 into another base", guilds=guilds)
+async def convert(interaction, startbase: app_commands.Range[int, 2, 36], targetbase: app_commands.Range[int, 2, 36],
+                  value: str):
+    l = len(value)
+    n = 0
+    value = value.upper()
+    for i in range(l - 1, -1, -1):
+        n += chars.index(value[i]) * startbase ** (l - 1 - i)
+    res = ""
+    while n:
+        res = chars[n % targetbase] + res
+        n = n // targetbase
+    await interaction.response.send_message(res)
 
 
 @bot.command()
@@ -91,11 +318,12 @@ async def h(ctx):
     embed = discord.Embed(title="Help", description="A Discord bot that uses deepl.", colour=0xB991FF,
                           timestamp=datetime.utcnow())
     embed.set_footer(text="Dislate")
-    embed.add_field(name="**__List of available target languages__**", value=help_text, inline=False)
+    embed.add_field(name="**__List of available source languages__**", value=help_text_source, inline=False)
+    embed.add_field(name="**__List of available target languages__**", value=help_text_target, inline=False)
     embed.add_field(name="**__Commands__**",
                     value="**say** *text* : Repeat what you say.\n**tr** *target_language* : Translate the message you are responding to into the target language.",
                     inline=False)
-    # await ctx.send("A Discord bot that uses deepl.\n\n : **\n\n\n**__Commands__**:\n)
+
     await ctx.send(embed=embed)
 
 
@@ -107,14 +335,30 @@ async def on_command_error(ctx, error):
 
 @bot.event
 async def on_ready():
-    # await tree.sync(guild=discord.Object(id=743319254107029584))
-    # await tree.sync(guild=discord.Object(id=1021336800079380480))
+    for guild in guilds:
+        await bot.tree.sync(guild=guild)
+    searchs = ["pat", "kiss", "hit", "slap", "happy", "sad", "angry", "hungry", "jealous", "innocent", "big brain time",
+               "sus", "cringe", "strange", "dab", "dealer"]
+    # try:
+    for i, search in enumerate(searchs):
+        r = requests.get("https://tenor.googleapis.com/v2/search?q={}&key={}&client_key={}&limit={}".format(search,
+                                                                                                            os.environ.get(
+                                                                                                                "TENOR"),
+                                                                                                            "Dislate",
+                                                                                                            50))
+        if r.status_code == 200:
+            all_gifs[searchs[i]] = json.loads(r.content)
+            # print(json.loads(r.content)["results"][40])
+            # print(gifs)
+    # except:
+    # pass
+    # await bot.tree.sync(guild=discord.Object(id=1021336800079380480))
     print("Logged in as {0.user}".format(bot))
 
 
-#keep_alive()
+# keep_alive()
 # bot.add_cog(Owner(bot))
 # bot.add_command(randomen)
 
 bot.run(str(os.environ.get("TOKEN", None)))
-# client.run(os.environ["token"])
+# client.run(str(os.environ.get("TOKEN", None)))
